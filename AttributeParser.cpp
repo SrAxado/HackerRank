8d2452a38758261d5242f7e89c6c721757ac29ce
SrAxado <sr.axado@gmail.com>
2024-08-14 20:04:01 +0100

    All attributes and values mapped properly

--- AttributeParser.cpp
+++ AttributeParser.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main() {
   int n, q;
   string line;
-  size_t pos;
+  size_t pos, pos_tmp;
   map<string, map<string, string> > hrml;
   string tag, attrib, value;
   
   cin >> n >> q;
+
   
   for (int i = 0; i <= n; ++i) {
     // string tag;
@@ -17,45 +18,101 @@ int main() {
     getline(cin, line);
 
     pos = line.find(' ');
-    tag = line.substr(1, pos);  // discards the first character, '<'
-    // hrml[tag]; // creates the tag set
 
-    line.erase(0, pos + 1);
+    if (pos != string::npos) {
+      // cout << pos << "<-----" << endl;
+      tag = line.substr(1, pos - 1);  // discards the first character, '<'
+      hrml[tag]; // creates the tag set
 
-    while ((pos = line.find(' ')) != string::npos) {
-      string sub_line = line.substr(0, pos);
-      size_t pos_tmp;
-      // string attrib, value;
+      line.erase(0, pos + 1); // line --> values / attributes + ">"
 
-      if ((pos_tmp = sub_line.find('=')) != string::npos) { // there's no space between attribute and value definition
-        attrib = sub_line.substr(0, pos_tmp);
-        value = sub_line.substr(pos_tmp + 1, sub_line.size());
-      
-      } else {  // there's space between the attribute and the value definition
-        pos_tmp = line.find('=');
-        attrib = sub_line;
-        // position 0 is equal in line and sub_line
-        pos = line.find(' ', pos_tmp + 2);  // finds next space after the "= " --> next definition position
-                  
-        value = line.substr(pos_tmp + 1, pos);
+      // cout << pos << "^ " << tag << "  --- " << line << endl;
+      while ((pos = line.find(' ')) != string::npos) {
+        // string attrib, value;
+        // size_t pos_tmp;
+        string sub_line = line.substr(0, pos);
+
+        // cout << "esta é a sub-line --> " << sub_line << "<--" << endl;
+
+        if ((pos_tmp = sub_line.find('"')) != string::npos) { // there's no space between attribute and value definition
+          // cout << "No space between attrib and values: " << sub_line << "<--" << endl;
+          attrib = sub_line.substr(0, pos_tmp - 1);
+          value = sub_line.substr(pos_tmp + 1, (sub_line.size() - 1) - (pos_tmp + 1));  // (size() - 1) - 1 <-- " char at the end
+        
+        } else {  // there's space between the attribute and the value definition
+          // simply don't use the attrib variable --> hrml[tag][sub_line] = value;
+          attrib = sub_line;  // sub_line.copy(attrib, sub_line.size(), 0);
+
+          pos_tmp = line.find('"');
+          // position 0 is equal in line and sub_line
+          pos = line.find(' ', pos_tmp);  // finds next space after the " char --> next definition position
+          if (pos != string::npos) {  // other attribute(s) exist
+            value = line.substr(pos_tmp + 1, (pos - 1) - (pos_tmp + 1));
+          } else {  // last value
+            value = line.substr(pos_tmp + 1, (line.size() - 2) - (pos_tmp + 1));  // (size() - 1)) - 1 ('"')
+            pos = line.size() - 2;  // to get ">" after erasing
+            // line.erase(0, line.size() - 1);
+          }
+        }
+        // hrml[tag][attrib] = value;
+        hrml[tag][attrib] = value;
+
+        line.erase(0, pos + 1);
+        
+        // cout << "-->"<< line << "<--" << endl;
+/*
+        if ((pos = line.find(' ')) != string::npos) {
+          cout << "Before new While-->" << line << "<--" << endl;
+          if ((pos_tmp = line.find('"')) != string::npos) {
+            attrib = line.substr(0, pos_tmp);
+            
+            pos = line.find('"');
+            value = line.substr(pos_tmp + 1, pos - (pos_tmp + 1));
+          }
+        } else {  // last value
+          cout << "Last value -->" << line << "<--" << endl;
+          if ((pos_tmp = line.find('"')) != string::npos) {
+            attrib = line.substr(0, pos_tmp);
+          }
+        }
+*/
       }
 
-      hrml[tag][attrib] = value;
+      if ((pos_tmp = line.find('"')) != string::npos) {
+        attrib = line.substr(0, pos_tmp - 1);
 
-      line.erase(0, pos + 1);
-      
-      cout << line << endl;
+        pos = line.find('"', pos_tmp + 1); // a second " always exists
+        value = line.substr(pos_tmp + 1, (pos - 1) - pos_tmp);
+
+        // cout << "line_size=" << line.size() << " pos=" << pos << " pos_tmp=" << pos_tmp << endl;
+
+        hrml[tag][attrib] = value;
+      }
+
+    } else {  // end tag || tag without attributes ("a tag can/may have no attributes")
+      // if ((pos = line.find('/')) == string::npos) { // tag without attributes
+      //   hrml[line.substr(1, line.size() - 2)];
+      // }
+
     }
 
     // map<string, string>::iterator it;
 
     // hrml.at("val")
 
-    cout << hrml["tag1"]["value"] << endl;
+
+    ///////cout << hrml["tag1"]["value"] << endl;
   }
   
   //cout << line << endl;
-  
+
+  for (const auto &[t, v] : hrml) {
+    cout << "hrml[" << t << "]" << endl;
+    for (const auto &[attrib, value] : v) {
+      cout << "\t(" << attrib << ", " << value << ");" << endl;
+    }
+  }
+
   return 0;
 }
 
@@ -68,3 +125,10 @@ int main() {
 // tag1.tag2~name
 // tag1~name
 // tag1~value
+
+// <tag1 value = "HelloWorld">
+// <tag2 name = "Name1" name2 = "Name2">
+// <tag3 another="another" final="final">
+// </tag3>
+// </tag2>
+// </tag1>
